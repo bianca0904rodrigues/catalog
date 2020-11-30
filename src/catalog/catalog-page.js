d64a829bc7e14c3bb424bfc9fa1da86b38181ded
Bianca Rodrigues <bianca0904rodrigues@gmail.com>
2020-11-30 16:31:30 -0300

    arquivos iniciais

---
+++ src/catalog/catalog-page.js
@@ -0,0 +1,10 @@
+import { Banner } from '../banner/banner.component'
+import { ListVideos } from '../list-videos/list-videos.component'
+
+export const Catalog = () => (
+  <>
+  <Banner /> 
+  <ListVideos />
+  <ListVideos />
+  </>
+)
---
+++ src/list-videos/list-videos.component.js
@@ -0,0 +1,3 @@
+export const ListVideos = () => (
+  <div>ListVideos</div>
+)
